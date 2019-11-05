a88d2c85b2c20df29d4974eb3fff1b5076df5def
Katerina <katerinalupacheva@gmail.com>
2019-11-05 17:10:30 +0300

    Updates

---
+++ precache-manifest.ca52974c5555df1b5bfdf179ab3a36e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5154bf849f2022c37814959f5c5f08ca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fd7ee93fb17a3aa22db",
+    "url": "/monsters-rolodex/static/css/main.06bdf97b.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "3fd7ee93fb17a3aa22db",
+    "url": "/monsters-rolodex/static/js/main.c962e427.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
